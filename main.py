88682ff75d24fa3e3b49553f6d212a1eb2588c0c
Will <w.j.colgate@gmail.com>
2025-04-06 12:51:57 +0800

    added extra options

--- main.py
+++ main.py
@@ -10,17 +10,24 @@ app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
 
+def radio_items(title, id, options, value):
+    buttons = html.Div([
+        html.H6(title),
+        dcc.RadioItems(options, value, id=id)
+    ])
+    return buttons
+
 app.layout = dbc.Container([
     html.Div(
         [
             html.Div(
                 [
-                    dcc.RadioItems(list(config.keys()), list(
-                        config.keys())[0], id='radio-selection'),
-                    dcc.RadioItems([72, 144, 216], 216, id='time_view'),
-                    dcc.RadioItems([24, 48, 72, 96, 120, 144], 144, id='lag'),
-                    dcc.RadioItems(['mark', 'spot', 'trade'],
-                                   'trade', id='ticker_type'),
+                    radio_items('Pairs', 'radio-selection', list(config.keys()), list(config.keys())[0]),
+                    radio_items('Time View', 'time_view', [72, 144, 216], 216),
+                    radio_items('Robust Spread Lag', 'lag', [24, 48, 72, 96, 120, 144], 144),
+                    radio_items('Ticker Type', 'ticker_type', ['mark', 'spot', 'trade'], 'trade'),
+                    radio_items('Resolution', 'resolution', ['15m', '30m', '1h', '4h', '12h', '1d'], '1h'),
+                    radio_items('Sigma', 'sigma', [0.5, 1, 1.5, 2],  2),
                 ],
                 style={'textAlign': 'left',
                        'display': 'flex',
@@ -28,11 +35,7 @@ app.layout = dbc.Container([
                        'align-items': 'flex-start'
                        },
             ),
-            dcc.Graph(id='graph-spread', responsive=True, style={'height': '80vh'})],
-        # style={'display': 'flex',
-        #        'flex-direction': 'column',
-        #        'align-items': 'center'
-        #        },
+            dcc.Graph(id='graph-spread', responsive=True, style={'height': '75vh'})],
     ),
     dcc.Interval(id='interval', interval=30000, n_intervals=0)
 ], style={'margin-top': 20})
@@ -45,27 +48,31 @@ app.layout = dbc.Container([
     Input('time_view', 'value'),
     Input('lag', 'value'),
     Input('ticker_type', 'value'),
+    Input('resolution', 'value'),
+    Input('sigma', 'value'),
 )
-def update_graph(value, n_intervals, interval, lag, ticker_type):
-    return update_pairs(value, interval, lag, ticker_type)
+def update_graph(value, n_intervals, interval, lag, ticker_type, resolution, sigma):
+    return update_pairs(value, interval, lag, ticker_type, resolution, sigma)
 
 
-def update_pairs(value, interval, lag, ticker_type):
+def update_pairs(value, interval, lag, ticker_type, resolution, sigma):
     entry = config[value]
     dff = data.create_pair_data(
-        entry['pair_1'], entry['pair_2'], '1h', entry['beta'], interval, ticker_type, lag)
+        entry['pair_1'], entry['pair_2'], resolution, entry['beta'], interval, ticker_type, lag)
     x_axis_labels = list(data.create_axis_from_df(dff))
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
                         vertical_spacing=0.05)
     fig.add_scatter(x=dff.index, y=dff['spread'],
                     row=1, col=1, showlegend=False, name='spread')
+    
     fig.add_scatter(x=dff.index, y=dff['robust'],
                     row=2, col=1, showlegend=False, name='robust')
     fig.add_hline(0, row=2, col=1, line_dash='dash', line_color='grey')
-    fig.add_hline(2, row=2, col=1, line_dash='dash', line_color='grey')
-    fig.add_hline(-2, row=2, col=1, line_dash='dash', line_color='grey')
+    fig.add_hline(sigma, row=2, col=1, line_dash='dash', line_color='grey')
+    fig.add_hline(-sigma, row=2, col=1, line_dash='dash', line_color='grey')
     fig.update_xaxes(range=[x_axis_labels[0], x_axis_labels[-1]])
     return fig
+
 
 
 if __name__ == '__main__':
